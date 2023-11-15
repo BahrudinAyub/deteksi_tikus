527fa413016863040a987b4739d1ea4f997a6d0f
BahrudinAyub <bahrudinayub86@gmail.com>
2023-11-15 13:52:22 +0700

    perubahan pada file deteksiTikus

--- DeteksiTikus.py
+++ DeteksiTikus.py
@@ -24,7 +24,7 @@ def extract_texture_features(image):
     return [avg_color[0], avg_color[1], avg_color[2], contrast, correlation, energy, homogeneity]
 
 # Load the test image membaca gambar
-test_image_path = 'D:/Kuliah/Project/Python/deteksi_tikus/dateset_tikus/datates/uji13.jpg'
+test_image_path = 'D:/Kuliah/Project/Python/deteksi_tikus/dateset_tikus/datates/uji12.jpg'
 test_image = cv2.imread(test_image_path)
 pil_test_image = Image.fromarray(cv2.cvtColor(test_image, cv2.COLOR_BGR2RGB))
 removed_bg_test_image = remove(pil_test_image).convert("RGB")
@@ -84,24 +84,29 @@ for contour in contours:
             eccentricity = np.sqrt(1 - (min(ellipse[1]) / max(ellipse[1])) ** 2)
 
             # Predict the category of the detected object using the trained model
-detected_object_features = [avg_color[0], avg_color[1], avg_color[2], contrast, correlation, energy,
-                            homogeneity, avg_hsv[0], avg_hsv[1], avg_hsv[2], ent]
+            detected_object_features = [avg_color[0], avg_color[1], avg_color[2], contrast, correlation, energy,
+                                        homogeneity, avg_hsv[0], avg_hsv[1], avg_hsv[2], ent]
 
-# Print the number of features in detected_object_features
-count_adatikus = len(detected_object_features)
-print(f"Jumlah fitur pada detected_object_features: {count_adatikus}")
+            # Print the number of features in detected_object_features
+            count_adatikus = len(detected_object_features)
+            print(f"Jumlah fitur pada detected_object_features: {count_adatikus}")
 
-# Check if the number of features matches the model
-if count_adatikus == count_adatikus:
-    predicted_category = model.predict([detected_object_features])
-    detected_objects.append({
-        'contour': contour,
-        'category': predicted_category[0],
-        'features': detected_object_features
-    })
-else:
-    print("Jumlah fitur pada detected_object_features tidak sesuai dengan model.")
-
+            # Check if the number of features matches the model
+            if count_adatikus == count_adatikus:
+                predicted_category = model.predict([detected_object_features])
+                detected_objects.append({
+                    'contour': contour,
+                    'category': predicted_category[0],
+                    'features': detected_object_features
+                })
+            else:
+                print("Jumlah fitur pada detected_object_features tidak sesuai dengan model.")
+        else:
+            # Default values when no contours are found
+            avg_color = np.array([0, 0, 0])
+            avg_hsv = np.array([0, 0, 0])
+            eccentricity = 0.0
+            ent = 0.0
 
 
 # Count the number of 'ada tikus' and 'tidak adatikus' eggs
